6eb663d90de9193a0039ecd548bb24b70536e461
Linh <trannhutlinh0925@gmail.com>
2022-08-20 11:23:44 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const req = require("express/lib/request");
-const ContactsRouter = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts", ContactsRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });

767dd4a2d8f970d460428408944d43dc35228183
Linh <trannhutlinh0925@gmail.com>
2022-08-20 11:20:34 +0700

    Cai dat express chay thong bao xin chao

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const req = require("express/lib/request");
+const ContactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", ContactsRouter);
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
